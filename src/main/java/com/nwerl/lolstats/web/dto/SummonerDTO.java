4f58154b7cf3bf296dfabe52f7b223cb9309a4f6
nwerl <msh6242006@gmail.com>
2021-01-18 01:43:13 +0900

    Add builder, toEntity to SummonerDTO

--- src/main/java/com/nwerl/lolstats/web/dto/SummonerDTO.java
+++ src/main/java/com/nwerl/lolstats/web/dto/SummonerDTO.java
@@ -1,17 +1,43 @@
 package com.nwerl.lolstats.web.dto;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import com.nwerl.lolstats.web.domain.summoner.Summoner;
+import lombok.Builder;
 
-@Setter
-@Getter
+import javax.xml.stream.FactoryConfigurationError;
+
+
 public class SummonerDTO {
-    private String accountId;
-    private Integer profileIconId;
-    private Long revisionDate;
-    private String name;
     private String id;
+    private String accountId;
     private String puuid;
+    private String name;
+
+
+    private Long profileIconId;
+    private Long revisionDate;
     private Long summonerLevel;
+
+    @Builder
+    public SummonerDTO(String id, String accountId, String puuid, String name,
+                       Long profileIconId, Long revisionDate, Long summonerLevel) {
+        this.id = id;
+        this.accountId = accountId;
+        this.puuid = puuid;
+        this.name = name;
+        this.profileIconId = profileIconId;
+        this.revisionDate = revisionDate;
+        this.summonerLevel = summonerLevel;
+    }
+
+    public Summoner toEntity() {
+        return Summoner.builder()
+                .id(id)
+                .accountId(accountId)
+                .puuid(puuid)
+                .name(name)
+                .profileIconId(profileIconId)
+                .revisionDate(revisionDate)
+                .summonerLevel(summonerLevel)
+                .build();
+    }
 }
