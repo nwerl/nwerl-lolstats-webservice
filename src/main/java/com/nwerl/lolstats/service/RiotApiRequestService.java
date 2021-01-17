894e41ced4754ad3547fcbc44d0230458a368df1
nwerl <msh6242006@gmail.com>
2021-01-18 01:40:58 +0900

    Rename Class name

--- src/main/java/com/nwerl/lolstats/service/RiotApiRequestService.java
+++ src/main/java/com/nwerl/lolstats/service/RiotApiRequestService.java
@@ -2,24 +2,19 @@ package com.nwerl.lolstats.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nwerl.lolstats.web.dto.SummonerDTO;
-import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
 @Service
-public class SummonerInfoService {
+public class RiotApiRequestService {
     private final String apiKey;
     private final ApiRequestService<String> apiRequestService;
     private final ObjectMapper objectMapper;
 
     @Autowired
-    public SummonerInfoService(@Value("${apikey}")String apiKey,
+    public RiotApiRequestService(@Value("${apikey}")String apiKey,
                                ApiRequestService<String> apiRequestService,
                                ObjectMapper objectMapper) {
         this.apiKey = apiKey;
