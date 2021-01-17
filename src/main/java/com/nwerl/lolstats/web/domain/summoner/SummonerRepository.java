8b3e34b349892ddcaf0867871dbd94bd06d79258
nwerl <msh6242006@gmail.com>
2021-01-18 01:54:24 +0900

    Remove needless import

--- src/main/java/com/nwerl/lolstats/web/domain/summoner/SummonerRepository.java
+++ src/main/java/com/nwerl/lolstats/web/domain/summoner/SummonerRepository.java
@@ -1,6 +1,4 @@
 package com.nwerl.lolstats.web.domain.summoner;
-
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
