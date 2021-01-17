25b887c64a2adbbbfb8beab3f90d6a4e80c7b9e6
nwerl <msh6242006@gmail.com>
2021-01-18 01:54:07 +0900

    Update RiotApiRequestController to return summonerID

--- src/main/java/com/nwerl/lolstats/web/RiotApiRequestController.java
+++ src/main/java/com/nwerl/lolstats/web/RiotApiRequestController.java
@@ -1,7 +1,7 @@
 package com.nwerl.lolstats.web;
 
-import com.nwerl.lolstats.service.SummonerInfoService;
-import com.nwerl.lolstats.web.dto.SummonerDTO;
+import com.nwerl.lolstats.service.RiotApiRequestService;
+import com.nwerl.lolstats.service.SummonerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class RiotApiRequestController {
-    private final SummonerInfoService summonerInfoService;
+    private final RiotApiRequestService riotApiRequestService;
+    private final SummonerService summonerService;
 
     @GetMapping("/api/v1/get/summonerinfo/{name}")
-    public SummonerDTO getSummonerInfo(@PathVariable String name) {
-        return summonerInfoService.getSummonerInfoByName(name);
+    public String saveSummonerInfo(@PathVariable String name) {
+        return summonerService.save(riotApiRequestService.getSummonerInfoByName(name));
     }
 }
