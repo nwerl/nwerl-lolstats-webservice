3ec60c2376e3fb7e3f6aa276291265ea98f3064d
nwerl <msh6242006@gmail.com>
2021-01-17 23:50:56 +0900

    Refactor URI for add name parameter
    can use name parameter by /api/v1/get/summonerinfo/{name}

--- src/main/java/com/nwerl/lolstats/web/RiotApiRequestController.java
+++ src/main/java/com/nwerl/lolstats/web/RiotApiRequestController.java
@@ -4,18 +4,16 @@ import com.nwerl.lolstats.service.SummonerInfoService;
 import com.nwerl.lolstats.web.dto.SummonerDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
 @RequiredArgsConstructor
 @RestController
 public class RiotApiRequestController {
     private final SummonerInfoService summonerInfoService;
 
-    @GetMapping("/api/v1/riot")
-    public SummonerDTO getSummonerInfo() throws IOException {
-        return summonerInfoService.getSummonerInfoByName("Vehumet");
+    @GetMapping("/api/v1/get/summonerinfo/{name}")
+    public SummonerDTO getSummonerInfo(@PathVariable String name) {
+        return summonerInfoService.getSummonerInfoByName(name);
     }
 }
