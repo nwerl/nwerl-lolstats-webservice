131fd03d337aa8a0c66758f14a6530e9887c17dc
nwerl <msh6242006@gmail.com>
2021-01-18 01:50:45 +0900

    Remove AutoIncreasing ID
    MongoDB doesn't need AutoIncreasing ID.
    Riot Api returns summoner's unique ID, so use it by unique ID.
    Don't use additional ID.

--- src/main/java/com/nwerl/lolstats/web/domain/summoner/Summoner.java
+++ src/main/java/com/nwerl/lolstats/web/domain/summoner/Summoner.java
@@ -3,34 +3,27 @@ package com.nwerl.lolstats.web.domain.summoner;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.*;
 
 @Data
 @Document(collection="summoner")
 public class Summoner {
-
-    @Id
-    private Long id;
-
-    private String summonerId;
+    private String id;
     private String accountId;
     private String puuid;
     private String name;
-
 
     private Long profileIconId;
     private Long revisionDate;
     private Long summonerLevel;
 
     @Builder
-    public Summoner(String summonerId, String accountId, String puuid, String name,
+    public Summoner(String id, String accountId, String puuid, String name,
                     Long profileIconId, Long revisionDate, Long summonerLevel) {
-        this.summonerId = summonerId;
+        this.id = id;
         this.accountId = accountId;
         this.puuid = puuid;
         this.name = name;
         this.profileIconId = profileIconId;
         this.revisionDate = revisionDate;
-        this.summonerId = summonerId;
     }
 }
