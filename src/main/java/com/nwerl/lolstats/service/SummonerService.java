31c4e54b899bd27c4ee6839c8ed850741f6b9487
nwerl <msh6242006@gmail.com>
2021-01-20 17:15:51 +0900

    Update SummonerService
    Add findByName and existsByName.
    findByName was constructed to check existsByName.
    if doesn't exist, call RiotApi and save to mongoDB.

--- src/main/java/com/nwerl/lolstats/service/SummonerService.java
+++ src/main/java/com/nwerl/lolstats/service/SummonerService.java
@@ -1,17 +1,34 @@
 package com.nwerl.lolstats.service;
 
 import com.nwerl.lolstats.web.domain.summoner.SummonerRepository;
-import com.nwerl.lolstats.web.dto.SummonerDTO;
+import com.nwerl.lolstats.web.dto.SummonerDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 
 @RequiredArgsConstructor
 @Service
 public class SummonerService {
     private final SummonerRepository summonerRepository;
+    private final RiotApiRequestService riotApiRequestService;
 
-    public String save(SummonerDTO summonerDTO) {
-        return summonerRepository.save(summonerDTO.toEntity()).getId();
+    public SummonerDto save(SummonerDto summonerDto) {
+        return new SummonerDto(summonerRepository.save(summonerDto.toEntity()));
     }
+
+    public Boolean existsByName(String name) {
+        return summonerRepository.existsByName(name);
+    }
+
+    public SummonerDto findByName(String name) {
+        SummonerDto summonerDto;
+        if(summonerRepository.existsByName(name)) {
+            summonerDto = new SummonerDto(summonerRepository.findByName(name));
+        }
+        else {
+            summonerDto = save(riotApiRequestService.getSummonerInfoByName(name));
+        }
+
+        return summonerDto;
+    }
+
 }
