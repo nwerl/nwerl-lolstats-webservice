b98f9ed99732f14f7e6964c0e8a7a946658de24c
nwerl <msh6242006@gmail.com>
2021-01-20 17:04:44 +0900

    Add MatchReferenceService to update matches

---
+++ src/main/java/com/nwerl/lolstats/service/MatchReferenceService.java
@@ -0,0 +1,29 @@
+package com.nwerl.lolstats.service;
+
+import com.mongodb.MongoException;
+import com.nwerl.lolstats.web.domain.match.MatchReferenceRepository;
+import com.nwerl.lolstats.web.dto.MatchReferenceDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class MatchReferenceService {
+    private final MatchReferenceRepository matchReferenceRepository;
+    private final RiotApiRequestService riotApiRequestService;
+    private final SummonerService summonerService;
+
+    public Boolean updateMatchReferenceByName(String name) {
+        try {
+            matchReferenceRepository.saveAll(riotApiRequestService.getMatchlistByName(summonerService.findByName(name).getName()).stream()
+                    .map(MatchReferenceDto::toEntity)
+                    .collect(Collectors.toList()));
+        }
+        catch (MongoException e) {
+            return false;
+        }
+        return true;
+    }
+}
