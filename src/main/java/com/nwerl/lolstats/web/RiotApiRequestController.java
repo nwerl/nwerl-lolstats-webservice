461072fa62b0f7c18f6256c3af3446723735c7b5
nwerl <msh6242006@gmail.com>
2021-01-21 22:51:27 +0900

    Update RiotApiRequestController to add getMatches

--- src/main/java/com/nwerl/lolstats/web/RiotApiRequestController.java
+++ src/main/java/com/nwerl/lolstats/web/RiotApiRequestController.java
@@ -2,11 +2,14 @@ package com.nwerl.lolstats.web;
 
 import com.nwerl.lolstats.service.MatchReferenceService;
 import com.nwerl.lolstats.service.SummonerService;
+import com.nwerl.lolstats.web.dto.MatchReferenceDto;
 import com.nwerl.lolstats.web.dto.SummonerDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,8 +22,13 @@ public class RiotApiRequestController {
         return summonerService.findByName(name);
     }
 
-        @GetMapping("/api/v1/get/matchlists/{name}")
-    public Boolean getMatchReferences(@PathVariable String name) {
+    @GetMapping("/api/v1/get/matchlists/{name}")
+    public List<MatchReferenceDto> getMatchReferences(@PathVariable String name) {
         return matchReferenceService.updateMatchReferenceByName(name);
+    }
+
+    @GetMapping("/api/v1/get/matches/{name}")
+    public Boolean getMatches(@PathVariable String name) throws InterruptedException {
+        return matchReferenceService.updateMatchByName(name);
     }
 }
