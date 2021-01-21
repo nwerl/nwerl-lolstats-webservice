5533f2f5725fde3022a493e9ab94d5bbb6929437
nwerl <msh6242006@gmail.com>
2021-01-21 22:56:37 +0900

    Update Service to update match by name
    updateMatchByName sleeps 1200 milis.
    If update 100 matches, It takes 2 minutes.
    Fix it with RiotApi's parameter.

--- src/main/java/com/nwerl/lolstats/service/MatchReferenceService.java
+++ src/main/java/com/nwerl/lolstats/service/MatchReferenceService.java
@@ -1,29 +1,41 @@
 package com.nwerl.lolstats.service;
 
 import com.mongodb.MongoException;
+import com.nwerl.lolstats.web.domain.match.Match;
+import com.nwerl.lolstats.web.domain.match.MatchReference;
 import com.nwerl.lolstats.web.domain.match.MatchReferenceRepository;
+import com.nwerl.lolstats.web.domain.match.MatchRepository;
 import com.nwerl.lolstats.web.dto.MatchReferenceDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class MatchReferenceService {
     private final MatchReferenceRepository matchReferenceRepository;
+    private final MatchRepository matchRepository;
     private final RiotApiRequestService riotApiRequestService;
     private final SummonerService summonerService;
 
-    public Boolean updateMatchReferenceByName(String name) {
-        try {
-            matchReferenceRepository.saveAll(riotApiRequestService.getMatchReferencesByName(summonerService.findByName(name).getName()).stream()
+    public List<MatchReferenceDto> updateMatchReferenceByName(String name) {
+       return matchReferenceRepository.saveAll(riotApiRequestService.getMatchReferencesByName(summonerService.findByName(name).getName()).stream()
                     .map(MatchReferenceDto::toEntity)
-                    .collect(Collectors.toList()));
+                    .collect(Collectors.toList())).stream()
+                                                .map(MatchReference::toDto)
+                                                .collect(Collectors.toList());
+    }
+
+    public Boolean updateMatchByName(String name) throws InterruptedException {
+        List<MatchReferenceDto> matchReferenceDtos = updateMatchReferenceByName(name);
+        for(MatchReferenceDto matchReferenceDto : matchReferenceDtos) {
+            matchRepository.save(riotApiRequestService.getMatchByGameId(matchReferenceDto.getGameId()));
+            Thread.sleep(1200);
         }
-        catch (MongoException e) {
-            return false;
-        }
+
         return true;
     }
 }
