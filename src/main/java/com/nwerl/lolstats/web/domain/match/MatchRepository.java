d035ab8e3980c76ad15c8bd9cf48c63de3fa27c1
nwerl <msh6242006@gmail.com>
2021-01-22 22:17:51 +0900

    Update MatchRepository to add existsByGameId

--- src/main/java/com/nwerl/lolstats/web/domain/match/MatchRepository.java
+++ src/main/java/com/nwerl/lolstats/web/domain/match/MatchRepository.java
@@ -1,6 +1,10 @@
 package com.nwerl.lolstats.web.domain.match;
 
+import com.mongodb.lang.Nullable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface MatchRepository extends MongoRepository<Match, String> {
+    public Match findTopByParticipantIdentities_Player_AccountId(String accountId);
+    public Boolean existsByGameId(Long gameId);
 }
