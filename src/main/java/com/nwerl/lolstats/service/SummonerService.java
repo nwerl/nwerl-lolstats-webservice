95dba7f7f4432969810aff4eb0b49f1b418955bf
nwerl <msh6242006@gmail.com>
2021-01-25 01:33:15 +0900

    Refactor SummonerService
    delete save, existsByName method

--- src/main/java/com/nwerl/lolstats/service/SummonerService.java
+++ src/main/java/com/nwerl/lolstats/service/SummonerService.java
@@ -11,21 +11,13 @@ public class SummonerService {
     private final SummonerRepository summonerRepository;
     private final RiotApiRequestService riotApiRequestService;
 
-    public SummonerDto save(SummonerDto summonerDto) {
-        return new SummonerDto(summonerRepository.save(summonerDto.toEntity()));
-    }
-
-    public Boolean existsByName(String name) {
-        return summonerRepository.existsByName(name);
-    }
-
     public SummonerDto findByName(String name) {
         SummonerDto summonerDto;
         if(summonerRepository.existsByName(name)) {
             summonerDto = new SummonerDto(summonerRepository.findByName(name));
         }
         else {
-            summonerDto = save(riotApiRequestService.getSummonerInfoByName(name));
+            summonerDto = new SummonerDto(summonerRepository.save(riotApiRequestService.getSummonerInfoByName(name).toEntity()));
         }
 
         return summonerDto;
