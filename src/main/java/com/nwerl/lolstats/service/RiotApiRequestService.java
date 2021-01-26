8a08f7d5fc293e145c80a9bf4cef2dc798b04047
nwerl <msh6242006@gmail.com>
2021-01-26 17:28:37 +0900

    Refactor RiotApiRequestService
    Don't inject RestTemplate from RestTemplateConfig.
    Inject RestTemplateBuilder from SpringBoot.
    Set RestTemplate in Constructor from RestTemplateBuilder.
    
    Don't use ObjectMapper and ApiService.
    Use getForObject request that get message and convert to object

--- src/main/java/com/nwerl/lolstats/service/RiotApiRequestService.java
+++ src/main/java/com/nwerl/lolstats/service/RiotApiRequestService.java
@@ -1,47 +1,52 @@
 package com.nwerl.lolstats.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nwerl.lolstats.web.domain.match.Match;
 import com.nwerl.lolstats.web.dto.MatchReferenceDto;
 import com.nwerl.lolstats.web.dto.MatchlistDto;
 import com.nwerl.lolstats.web.dto.SummonerDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.Charset;
+import java.time.Duration;
 import java.util.List;
 
 @Service
 public class RiotApiRequestService {
     private final String apiKey;
-    private final ApiRequestService<String> apiRequestService;
-    private final ObjectMapper objectMapper;
+    private final RestTemplate restTemplate;
 
     @Autowired
     public RiotApiRequestService(@Value("${apikey}")String apiKey,
-                                 ApiRequestService<String> apiRequestService,
-                                 ObjectMapper objectMapper) {
+                                 RestTemplateBuilder restTemplateBuilder) {
         this.apiKey = apiKey;
-        this.apiRequestService = apiRequestService;
-        this.objectMapper = objectMapper;
+        this.restTemplate = restTemplateBuilder.requestFactory(()->new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
+                .setConnectTimeout(Duration.ofMillis(20000))
+                .setReadTimeout(Duration.ofMillis(20000))
+                .additionalMessageConverters(new StringHttpMessageConverter(Charset.forName("UTF-8")))
+                .build();
     }
 
     public SummonerDto getSummonerInfoByName(String name) {
         String url = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + apiKey;
-        return objectMapper.convertValue(apiRequestService.get(url, HttpHeaders.EMPTY).getBody(), SummonerDto.class);
+        return restTemplate.getForObject(url, SummonerDto.class);
     }
 
     public List<MatchReferenceDto> getMatchReferencesByAccountId(String accountId) {
         String url = "https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/"
                 + accountId
                 + "?api_key=" + apiKey;
-        System.out.println(url);
-        return objectMapper.convertValue(apiRequestService.get(url, HttpHeaders.EMPTY).getBody(), MatchlistDto.class).getMatches();
+        return restTemplate.getForObject(url, MatchlistDto.class).getMatches();
     }
 
     public Match getMatchByGameId(Long gameId) {
         String url = "https://kr.api.riotgames.com/lol/match/v4/matches/" + gameId + "?api_key=" + apiKey;
-        return objectMapper.convertValue(apiRequestService.get(url, HttpHeaders.EMPTY).getBody(), Match.class);
+        return restTemplate.getForObject(url, Match.class);
     }
 }
