09cf34e2f34f7c0c159684ba6e72f157ad7b2441
nwerl <msh6242006@gmail.com>
2021-01-28 14:41:43 +0900

    Add Spring Batch

--- src/main/java/com/nwerl/lolstats/Application.java
+++ src/main/java/com/nwerl/lolstats/Application.java
@@ -1,8 +1,10 @@
 package com.nwerl.lolstats;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
