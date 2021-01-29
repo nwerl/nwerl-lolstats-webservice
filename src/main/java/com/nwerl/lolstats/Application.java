14ee8d4c637d26357a5f1c2de0348e1374ea0b9a
nwerl <msh6242006@gmail.com>
2021-01-29 20:45:41 +0900

    Update Application to remove Batch Annotation

--- src/main/java/com/nwerl/lolstats/Application.java
+++ src/main/java/com/nwerl/lolstats/Application.java
@@ -1,13 +1,11 @@
 package com.nwerl.lolstats;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableBatchProcessing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class);
     }
 }
