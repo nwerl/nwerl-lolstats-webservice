c30b4cbadad5b2948250d335cfb77b296dbf03d5
nwerl <msh6242006@gmail.com>
2021-02-04 17:04:30 +0900

    Remove MatchReferenceRepository

--- src/main/java/com/nwerl/lolstats/web/domain/match/MatchReferenceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nwerl.lolstats.web.domain.match;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface MatchReferenceRepository extends MongoRepository<MatchReference, Integer> {
-    public List<MatchReference> findAll();
-}
