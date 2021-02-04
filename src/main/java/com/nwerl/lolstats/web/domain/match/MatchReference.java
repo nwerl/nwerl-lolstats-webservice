5e396187ba4054714cae40c65eb4a569c491d414
nwerl <msh6242006@gmail.com>
2021-02-04 17:03:34 +0900

    Refactor MatchReference
    Remove needless fields.

--- src/main/java/com/nwerl/lolstats/web/domain/match/MatchReference.java
+++ src/main/java/com/nwerl/lolstats/web/domain/match/MatchReference.java
@@ -1,35 +1,16 @@
 package com.nwerl.lolstats.web.domain.match;
 
-import com.nwerl.lolstats.web.dto.MatchReferenceDto;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Builder
-@Data
-@Document(collection = "matchReference")
+@AllArgsConstructor
+@Getter
 public class MatchReference {
     @Id
     private Long gameId;
-    private String role;
-    private Integer season;
-    private String platformId;
-    private Integer champion;
-    private Integer queue;
-    private String lane;
-    private Long timestamp;
-
-    public MatchReferenceDto toDto() {
-        return MatchReferenceDto.builder()
-                .gameId(gameId)
-                .role(role)
-                .season(season)
-                .platformId(platformId)
-                .champion(champion)
-                .queue(queue)
-                .lane(lane)
-                .timestamp(timestamp)
-                .build();
-    }
+    private Long timeStamp;
 }
