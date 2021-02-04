4ce6ab1c26ea5a6ebc3134ce7fb73a83de4ee1e2
nwerl <msh6242006@gmail.com>
2021-02-04 17:01:17 +0900

    Update MatchListDto to add @Builder

--- src/main/java/com/nwerl/lolstats/web/dto/riotApi/matchreference/MatchListDto.java
+++ src/main/java/com/nwerl/lolstats/web/dto/riotApi/matchreference/MatchListDto.java
@@ -1,12 +1,14 @@
-package com.nwerl.lolstats.web.dto;
+package com.nwerl.lolstats.web.dto.riotApi.matchreference;
 
+import lombok.Builder;
 import lombok.Data;
 
 
 import java.util.List;
 
+@Builder
 @Data
-public class MatchlistDto {
+public class MatchListDto {
     private Integer startIndex;
     private Integer totalGames;
     private Integer endIndex;
