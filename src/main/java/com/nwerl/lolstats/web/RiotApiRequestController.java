308c867de50f91623f900781e3a1ee11e1de13cc
nwerl <msh6242006@gmail.com>
2021-02-04 16:52:51 +0900

    Update RiotApiRequestController
    Fix method name.
    Fix return type.
    Add getChallengerLeagueItems.

--- src/main/java/com/nwerl/lolstats/web/RiotApiRequestController.java
+++ src/main/java/com/nwerl/lolstats/web/RiotApiRequestController.java
@@ -1,12 +1,14 @@
 package com.nwerl.lolstats.web;
 
 import com.nwerl.lolstats.service.FeaturedGameInfoService;
+import com.nwerl.lolstats.service.LeagueService;
 import com.nwerl.lolstats.service.MatchService;
 import com.nwerl.lolstats.service.SummonerService;
-import com.nwerl.lolstats.web.domain.match.FeaturedGameInfo;
-import com.nwerl.lolstats.web.dto.FeaturedGameInfoDto;
-import com.nwerl.lolstats.web.dto.MatchReferenceDto;
-import com.nwerl.lolstats.web.dto.SummonerDto;
+import com.nwerl.lolstats.web.dto.riotApi.featuredgame.FeaturedGameInfoDto;
+import com.nwerl.lolstats.web.dto.riotApi.match.MatchDto;
+import com.nwerl.lolstats.web.dto.riotApi.matchreference.MatchReferenceDto;
+import com.nwerl.lolstats.web.dto.riotApi.summoner.SummonerDto;
+import com.nwerl.lolstats.web.dto.riotApi.league.LeagueItemDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +22,7 @@ public class RiotApiRequestController {
     private final SummonerService summonerService;
     private final MatchService matchService;
     private final FeaturedGameInfoService featuredGameInfoService;
+    private final LeagueService leagueService;
 
     @GetMapping("/api/v1/get/summonerinfo/{name}")
     public SummonerDto getSummonerInfo(@PathVariable String name) {
@@ -27,17 +30,22 @@ public class RiotApiRequestController {
     }
 
     @GetMapping("/api/v1/get/matchlists/{name}")
-    public List<MatchReferenceDto> getMatchReferences(@PathVariable String name) {
-        return matchService.updateMatchReferenceByName(name);
+    public MatchReferenceDto getLastMatchReference(@PathVariable String name) {
+        return matchService.getLastMatchReferenceByName(name);
     }
 
     @GetMapping("/api/v1/get/matches/{name}")
-    public Boolean getMatches(@PathVariable String name) {
+    public MatchDto getMatches(@PathVariable String name) {
         return matchService.updateMatchByName(name);
     }
 
     @GetMapping("/api/v1/get/featured-games")
     public List<FeaturedGameInfoDto> getFeaturedGames() {
         return featuredGameInfoService.updateFeaturedGames();
+    }
+
+    @GetMapping("/api/v1/get/leagues/challenger")
+    public List<LeagueItemDto> getChallengerLeagueItems() {
+        return leagueService.updateChallengerLeagueList().getEntries();
     }
 }
