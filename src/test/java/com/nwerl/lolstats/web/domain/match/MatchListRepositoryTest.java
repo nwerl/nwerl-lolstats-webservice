b5144d9b31e16814e888a391ef993c7f135657fe
nwerl <msh6242006@gmail.com>
2021-02-09 23:35:29 +0900

    Update MatchListRepositoryTest
    Add mongoTemplate_Test

--- src/test/java/com/nwerl/lolstats/web/domain/match/MatchListRepositoryTest.java
+++ src/test/java/com/nwerl/lolstats/web/domain/match/MatchListRepositoryTest.java
@@ -1,17 +1,29 @@
 package com.nwerl.lolstats.web.domain.match;
 
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.CriteriaDefinition;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringRunner.class)
 @DataMongoTest
 public class MatchListRepositoryTest {
+    @Autowired
+    private MongoTemplate mongoTemplate;
     @Autowired
     private MatchListRepository matchListRepository;
 
@@ -21,19 +33,25 @@ public class MatchListRepositoryTest {
         System.out.println(matchListRepository.findBySummonerName(summonerName,0, 20).getMatchReferences().get(0).getGameId());
     }
 
-//    @Test
-//    public void matchListWriter_Test(){
-//        String accountId = "CuufRXm4Cm2ITWbgem3fbGGXCdAhSFsPCEPF-5wxEpzPVYw";
-//        Integer prevSize = matchListRepository.findByAccountId(accountId).getMatchReferences().size();
-//
-//        List<MatchReference> list = new ArrayList<>();
-//        list.add(MatchReference.builder().gameId(123456L).build());
-//
-//        MatchList matchList = matchListRepository.findByAccountId(accountId);
-//        matchList.addMatchReferences(list);
-//
-//        System.out.println(matchListRepository.findByAccountId(accountId).getMatchReferences().size());
-//
-//        //assertThat(prevSize+1, is(matchListRepository.findByAccountId(accountId).getMatchReferences().size()+1));
-//    }
+    @Test
+    public void mongoTemplate_Test() {
+        //given
+        int prevSize = matchListRepository.findByAccountId("BNHbH704a7gx2MANguAPq64KNErxxmFhic9ti-PHGUxS8Lk")
+                .getMatchReferences().size();
+        String accountId = "BNHbH704a7gx2MANguAPq64KNErxxmFhic9ti-PHGUxS8Lk";
+        MatchReference input = MatchReference.builder().gameId(888L).timeStamp(1234L).build();
+        Query query = new Query(Criteria.where("_id").is(accountId));
+        Update update = new Update().addToSet("matchReferences", input);
+
+        //when
+        mongoTemplate.findAndModify(query, update, FindAndModifyOptions.options().upsert(true), MatchList.class);
+
+        //then
+        assertThat(prevSize+1, is(matchListRepository.findByAccountId("BNHbH704a7gx2MANguAPq64KNErxxmFhic9ti-PHGUxS8Lk")
+                .getMatchReferences().size()));
+
+        mongoTemplate.updateMulti(query,
+                new Update().pull("matchReferences", Query.query(Criteria.where("_id").is(888L))),
+                MatchList.class);
+    }
 }
