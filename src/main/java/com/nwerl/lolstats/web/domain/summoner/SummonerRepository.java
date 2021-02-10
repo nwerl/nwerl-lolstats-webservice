19350c875457ab08bff7f3fb537a64855bac1102
nwerl <msh6242006@gmail.com>
2021-02-10 22:07:23 +0900

    Update SummonerRepository
    Add findById method.

--- src/main/java/com/nwerl/lolstats/web/domain/summoner/SummonerRepository.java
+++ src/main/java/com/nwerl/lolstats/web/domain/summoner/SummonerRepository.java
@@ -2,9 +2,11 @@ package com.nwerl.lolstats.web.domain.summoner;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SummonerRepository extends MongoRepository<Summoner, String> {
     public List<Summoner> findAll();
     public Summoner findByName(String name);
+    public Optional<Summoner> findById(String id);
     public Boolean existsByName(String name);
 }
