7bcff7d002789f42e6a0a7fcbeaa97fb8c955325
nwerl <msh6242006@gmail.com>
2021-02-10 22:00:17 +0900

    Update IndexController
    Add summonerMatchList view.

--- src/main/java/com/nwerl/lolstats/web/IndexController.java
+++ src/main/java/com/nwerl/lolstats/web/IndexController.java
@@ -1,18 +1,29 @@
 package com.nwerl.lolstats.web;
 
+import com.nwerl.lolstats.service.LeagueService;
+import com.nwerl.lolstats.service.MatchService;
 import com.nwerl.lolstats.service.SummonerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
-    private final SummonerService summonerService;
+    private final LeagueService leagueService;
+    private final MatchService matchService;
 
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("leagueList", leagueService.getLeagueRanking());
+        return "index";
+    }
+
+    @GetMapping("/summoner/{summonerName}/matchList")
+    public String summonerMatchList(Model model, @PathVariable String summonerName) {
+        model.addAttribute("matchList", matchService);
         return "index";
     }
 }
