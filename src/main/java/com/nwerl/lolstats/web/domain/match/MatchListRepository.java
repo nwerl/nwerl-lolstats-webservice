7d5cfab547cb09233286a3f4c31cfe30f1288a6b
nwerl <msh6242006@gmail.com>
2021-02-10 22:04:37 +0900

    Add CustomRepository

--- src/main/java/com/nwerl/lolstats/web/domain/match/MatchListRepository.java
+++ src/main/java/com/nwerl/lolstats/web/domain/match/MatchListRepository.java
@@ -1,11 +1,17 @@
 package com.nwerl.lolstats.web.domain.match;
 
+import com.nwerl.lolstats.web.domain.custom.CustomRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
-public interface MatchListRepository extends MongoRepository<MatchList, String> {
+public interface MatchListRepository extends MongoRepository<MatchList, String>, CustomRepository {
     public List<MatchList> findAll();
     public MatchList findByAccountId(String Id);
     public Boolean existsByAccountId(String Id);
+
+    @Query(value = "{summonerName: ?0}", fields = "{matchReferences: {$slice: [?1, ?2]}}")
+    public MatchList findBySummonerName(String summonerName, int skip, int limit);
+    public void customMethod(String accountId, MatchReference matchReference);
 }
