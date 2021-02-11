e8c3e222c2b1b27186887ff355f260aeeb722610
nwerl <msh6242006@gmail.com>
2021-02-11 23:31:18 +0900

    Update MatchRepository to add findById method

--- src/main/java/com/nwerl/lolstats/web/domain/match/MatchRepository.java
+++ src/main/java/com/nwerl/lolstats/web/domain/match/MatchRepository.java
@@ -1,9 +1,11 @@
 package com.nwerl.lolstats.web.domain.match;
 
-import com.mongodb.lang.Nullable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface MatchRepository extends MongoRepository<Match, String> {
+import java.util.Optional;
+
+public interface MatchRepository extends MongoRepository<Match, Long> {
     //public Match findTopByParticipantIdentities_Player_AccountId(String accountId);
     public Boolean existsByGameId(Long gameId);
+    public Optional<Match> findById(Long id);
 }
