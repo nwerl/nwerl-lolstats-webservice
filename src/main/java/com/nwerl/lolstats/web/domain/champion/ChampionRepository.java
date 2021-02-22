d539e50765135a0645684ad7272eb71d3db82637
nwerl <msh6242006@gmail.com>
2021-02-23 02:03:07 +0900

    Remove Champion

--- src/main/java/com/nwerl/lolstats/web/domain/champion/ChampionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.nwerl.lolstats.web.domain.champion;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ChampionRepository extends MongoRepository<Champion, Long> {
-}
