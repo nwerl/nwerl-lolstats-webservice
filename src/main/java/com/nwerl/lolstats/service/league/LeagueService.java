6f1aef052ee2ec557f5119ff275b72d4cf3763ac
nwerl <msh6242006@gmail.com>
2021-02-28 21:29:01 +0900

    Rename LeagueListDto

--- src/main/java/com/nwerl/lolstats/service/league/LeagueService.java
+++ src/main/java/com/nwerl/lolstats/service/league/LeagueService.java
@@ -2,19 +2,12 @@ package com.nwerl.lolstats.service.league;
 
 import com.nwerl.lolstats.web.domain.league.LeagueItem;
 import com.nwerl.lolstats.web.domain.league.LeagueItemRepository;
-import com.nwerl.lolstats.web.dto.riotApi.league.LeagueItemDto;
-import com.nwerl.lolstats.web.dto.riotApi.league.LeagueListDto;
-import com.nwerl.lolstats.web.dto.riotApi.league.LeagueRankingDto;
+import com.nwerl.lolstats.web.dto.riotapi.league.RiotLeagueItemDto;
+import com.nwerl.lolstats.web.dto.riotapi.league.RiotLeagueListDto;
+import com.nwerl.lolstats.web.dto.view.LeagueRankingDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,17 +21,21 @@ public class LeagueService {
     private final LeagueItemRepository leagueItemRepository;
     private final LeagueApiCaller leagueApiCaller;
 
-    public LeagueListDto findAll() {
-        return new LeagueListDto(leagueItemRepository.findAll().stream().map(LeagueItem::of).collect(Collectors.toList()));
+    public RiotLeagueListDto findAll() {
+        return new RiotLeagueListDto(leagueItemRepository.findAll().stream().map(LeagueItem::of).collect(Collectors.toList()));
     }
 
     //ChallengerLeagueList를 갱신함.
-    public LeagueListDto updateChallengerLeagueList() {
-        List<LeagueItemDto> list = leagueApiCaller.callApiChallengerLeagueItem().getEntries();
+    public RiotLeagueListDto updateChallengerLeagueList() {
+        List<RiotLeagueItemDto> list = leagueApiCaller.fetchChallengerLeagueListFromRiotApi().getEntries();
         leagueItemRepository.deleteAll(); //deleteAll 안하려면 N^2로 대조하여 받아온 list에서 없는 DB 데이터 삭제해야 함.
-        leagueItemRepository.saveAll(list.stream().map(LeagueItemDto::toEntity).collect(Collectors.toList()));
+        leagueItemRepository.saveAll(list.stream().map(RiotLeagueItemDto::toEntity).collect(Collectors.toList()));
 
-        return new LeagueListDto(list);
+        return new RiotLeagueListDto(list);
+    }
+
+    public RiotLeagueListDto fetchChallengerLeagueListFromRiotApi() {
+        return leagueApiCaller.fetchChallengerLeagueListFromRiotApi();
     }
 
 
