961ac7e77ff1d37a5ff0efc1d6ee347dcf3fb9de
nwerl <msh6242006@gmail.com>
2021-03-02 23:46:16 +0900

    Refactor BatchApplication
    Don't use accountId and SummonerName jobParameters

--- src/main/java/com/nwerl/lolstats/batch/BatchApplication.java
+++ src/main/java/com/nwerl/lolstats/batch/BatchApplication.java
@@ -1,11 +1,11 @@
 package com.nwerl.lolstats.batch;
 
 import com.nwerl.lolstats.service.league.LeagueService;
-import com.nwerl.lolstats.service.summoner.SummonerService;
-import com.nwerl.lolstats.web.dto.riotapi.league.RiotLeagueItemDto;
-import com.nwerl.lolstats.web.dto.riotapi.league.RiotLeagueListDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.*;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,8 +13,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Slf4j
@@ -25,48 +23,35 @@ public class BatchApplication {
     private final Job leagueJob;
 
     private final LeagueService leagueService;
-    private final SummonerService summonerService;
-
-    private RiotLeagueListDto leagueList;
-    private Map<String, String> accountIdMap;
 
     @Autowired
     public BatchApplication(JobLauncher jobLauncher,
                             LeagueService leagueService,
-                            SummonerService summonerService,
                             @Qualifier("matchJob") Job matchJob,
                             @Qualifier("leagueJob") Job leagueJob) {
         this.jobLauncher = jobLauncher;
         this.leagueService = leagueService;
         this.matchJob = matchJob;
         this.leagueJob = leagueJob;
-        this.summonerService = summonerService;
-        accountIdMap = new HashMap<>();
     }
 
     @Scheduled(fixedDelay = 3600000)
     public void leagueLaunch() throws Exception {
         log.info("Job Started at : {}", new Date());
+
+        leagueService.deleteAll();
+
         JobParameters param = new JobParametersBuilder()
                 .addString("dateTime", String.valueOf(System.currentTimeMillis()))
                 .toJobParameters();
         JobExecution execution = jobLauncher.run(leagueJob, param);
-
-        leagueList = this.leagueService.findAll();
-        for(RiotLeagueItemDto item : leagueList.getEntries()) {
-            accountIdMap.put(item.getSummonerName(), summonerService.findAccountIdById(item.getSummonerId()));
-        }
     }
 
-    @Scheduled(fixedRateString = "3000", initialDelay = 5000)
+    @Scheduled(fixedRateString = "1400", initialDelay = 5000)
     public void matchLaunch() throws Exception {
         log.info("Job Started at : {}", new Date());
 
-        String summonerName = leagueList.traversal().getSummonerName();
-
         JobParameters param = new JobParametersBuilder()
-                .addString("accountId",  accountIdMap.get(summonerName))
-                .addString("summonerName", summonerName)
                 .addString("dateTime", String.valueOf(System.currentTimeMillis()))
                 .toJobParameters();
         JobExecution execution = jobLauncher.run(matchJob, param);
