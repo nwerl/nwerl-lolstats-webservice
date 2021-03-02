3749636e31ae2405d02cbbd5a44d9062008c4a39
nwerl <msh6242006@gmail.com>
2021-03-02 23:40:27 +0900

    Add MatchIdSet

---
+++ src/main/java/com/nwerl/lolstats/batch/MatchIdSet.java
@@ -0,0 +1,30 @@
+package com.nwerl.lolstats.batch;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Component
+public class MatchIdSet {
+    private Set<Long> matchIdSet = new LinkedHashSet<>();
+
+    public Long getNextMatchId() {
+        Iterator<Long> it = matchIdSet.iterator();
+
+        if(!it.hasNext())   return null;
+        Long nextMatchId = it.next();
+        it.remove();
+
+        return nextMatchId;
+    }
+
+    public void addMatchId(Long matchId) {
+        matchIdSet.add(matchId);
+    }
+
+    public boolean noMatchesToUpdate() {
+        return matchIdSet.isEmpty();
+    }
+}
