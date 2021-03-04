715e060206ce572bbe55750be3039e70d74c15fc
nwerl <msh6242006@gmail.com>
2021-03-04 23:33:24 +0900

    Add QueueType

---
+++ src/main/java/com/nwerl/lolstats/web/dto/riotapi/matchreference/QueueType.java
@@ -0,0 +1,11 @@
+package com.nwerl.lolstats.web.dto.riotapi.matchreference;
+
+public class QueueType {
+    public static final Integer SOLO_RANK = 420;
+    public static final Integer NORMAL = 430;
+    public static final Integer TEAM_RANK = 440;
+    public static final Integer ARAM = 450;
+
+
+    private QueueType() {}
+}
