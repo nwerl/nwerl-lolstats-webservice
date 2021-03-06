871a0c1c73e5636fbd9108b0a1058eeac98c05e0
nwerl <msh6242006@gmail.com>
2021-03-06 21:29:18 +0900

    Refactor MatchListProcessor and MatchListReader
    Set gameId when not exists in DB.

--- src/main/java/com/nwerl/lolstats/batch/matchlist/MatchListProcessor.java
+++ src/main/java/com/nwerl/lolstats/batch/matchlist/MatchListProcessor.java
@@ -1,13 +1,15 @@
 package com.nwerl.lolstats.batch.matchlist;
 
 
-import com.nwerl.lolstats.batch.MatchIdSet;
 import com.nwerl.lolstats.service.match.MatchService;
 import com.nwerl.lolstats.web.domain.match.MatchList;
 import com.nwerl.lolstats.web.domain.match.MatchReference;
 import com.nwerl.lolstats.web.dto.riotapi.matchreference.RiotMatchReferenceDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
@@ -20,18 +22,25 @@ import java.util.Collections;
 @Component
 public class MatchListProcessor implements ItemProcessor<RiotMatchReferenceDto, MatchList> {
     private final MatchService matchService;
-    private final MatchIdSet matchIdSet;
+    private StepExecution stepExecution;
 
     @Override
     public MatchList process(RiotMatchReferenceDto item) throws Exception {
         Long gameId = item.getGameId();
-        Long timeStamp = item.getTimestamp();
 
         if(!matchService.existsByGameId(gameId)) {
             log.info("{}'s GAME {} NOT EXISTS IN DB", item.getAccountId(), gameId);
-            matchIdSet.addMatchId(gameId);
+            stepExecution.getJobExecution().getExecutionContext().put("GAME_ID", gameId);
+        }
+        else {
+            stepExecution.setExitStatus(ExitStatus.FAILED);
         }
 
-        return MatchList.builder().accountId(item.getAccountId()).matchReferences(Collections.singletonList(new MatchReference(gameId, timeStamp))).build();
+        return MatchList.builder().accountId(item.getAccountId()).matchReferences(Collections.singletonList(new MatchReference(gameId, item.getTimestamp()))).build();
+    }
+
+    @BeforeStep
+    public void setUp(StepExecution stepExecution) {
+        this.stepExecution = stepExecution;
     }
 }
--- src/main/java/com/nwerl/lolstats/batch/matchlist/MatchListReader.java
+++ src/main/java/com/nwerl/lolstats/batch/matchlist/MatchListReader.java
@@ -1,19 +1,18 @@
 package com.nwerl.lolstats.batch.matchlist;
 
-import com.nwerl.lolstats.service.league.LeagueService;
+import com.nwerl.lolstats.batch.LeagueItemAccountIdList;
 import com.nwerl.lolstats.service.match.MatchService;
-import com.nwerl.lolstats.service.summoner.SummonerService;
-import com.nwerl.lolstats.web.dto.riotapi.league.RiotLeagueItemDto;
 import com.nwerl.lolstats.web.dto.riotapi.matchreference.RiotMatchReferenceDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,34 +20,40 @@ import java.util.stream.Collectors;
 @Component
 public class MatchListReader implements ItemReader<RiotMatchReferenceDto> {
     private final MatchService matchService;
-    private final LeagueService leagueService;
-    private final SummonerService summonerService;
+    private final LeagueItemAccountIdList leagueItemAccountIdList;
+    private StepExecution stepExecution;
 
-    private Queue<String> leagueItemAccountIdQueue;
+    private String accountId;
 
     @Override
-    public RiotMatchReferenceDto read() throws Exception{
-        if(leagueItemAccountIdQueueIsNotInitialized()) {
-            setLeagueItemAccountIdQueue();
-        }
+    public RiotMatchReferenceDto read() throws InterruptedException {
+        if(thisAccountIdIsUpdated())
+            return null;
 
-        String accountId = leagueItemAccountIdQueue.poll();
-        if(accountId == null)   return null;
+        accountId = leagueItemAccountIdList.getNextAccountId();
+        RiotMatchReferenceDto matchReference = getLastMatchReference(accountId);
 
-        return getLastMatchReference(accountId);
+        if(matchReference == null)
+            stepExecution.setExitStatus(ExitStatus.FAILED);
+
+        return matchReference;
     }
 
     private RiotMatchReferenceDto getLastMatchReference(String accountId) throws InterruptedException {
-        return matchService.fetchLastRankMatchReferenceFromRiotApi(accountId);
+        try {
+            return matchService.fetchLastRankMatchReferenceFromRiotApi(accountId);
+        } catch (HttpClientErrorException.TooManyRequests e) {
+            Thread.sleep(120000 + 20000);
+            return matchService.fetchLastRankMatchReferenceFromRiotApi(accountId);
+        }
     }
 
-    private Boolean leagueItemAccountIdQueueIsNotInitialized() {
-        return leagueItemAccountIdQueue == null;
+    private Boolean thisAccountIdIsUpdated() {
+        return accountId != null;
     }
 
-    private void setLeagueItemAccountIdQueue() {
-        leagueItemAccountIdQueue = new LinkedList<>(summonerService.findAccountIdListByIdList(
-                leagueService.findAll().getEntries().stream()
-                .map(RiotLeagueItemDto::getSummonerId).collect(Collectors.toList())));
+    @BeforeStep
+    public void setUp(StepExecution stepExecution) {
+        this.stepExecution = stepExecution;
     }
 }
