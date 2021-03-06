fb852bb18d499e1395e79330ad5c1bb55efa5d94
nwerl <msh6242006@gmail.com>
2021-03-06 21:28:34 +0900

    Refactor MatchReader
    MatchReader receive gameId from executionContext.

--- src/main/java/com/nwerl/lolstats/batch/match/MatchReader.java
+++ src/main/java/com/nwerl/lolstats/batch/match/MatchReader.java
@@ -1,13 +1,16 @@
 package com.nwerl.lolstats.batch.match;
 
-import com.nwerl.lolstats.batch.MatchIdSet;
 import com.nwerl.lolstats.service.match.MatchService;
 import com.nwerl.lolstats.web.dto.riotapi.match.RiotMatchDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,17 +18,42 @@ import org.springframework.stereotype.Component;
 @Component
 public class MatchReader implements ItemReader<RiotMatchDto> {
     private final MatchService matchService;
-    private final MatchIdSet matchIdSet;
+    private StepExecution stepExecution;
+
+    private Long gameId;
 
     @Override
-    public RiotMatchDto read() {
-        if (matchIdSet.noMatchesToUpdate())
+    public RiotMatchDto read() throws InterruptedException {
+        if (thisGameIdIsUpdated())
             return null;
-        log.info("matchIdSet size : {}", matchIdSet.getSize());
-        Long nextMatchId = matchIdSet.getNextMatchId();
 
-        RiotMatchDto nextMatchItem = matchService.fetchMatchFromRiotApi(nextMatchId);
+        gameId = getGameId();
 
-        return nextMatchItem;
+        return fetchMatchFromRiotApi(gameId);
+    }
+
+    public Long getGameId() {
+        ExecutionContext jobExecutionContext = stepExecution.getJobExecution().getExecutionContext();
+
+        return jobExecutionContext.getLong("GAME_ID");
+    }
+
+    private boolean thisGameIdIsUpdated() {
+        return gameId != null;
+    }
+
+    public RiotMatchDto fetchMatchFromRiotApi(Long gameId) throws InterruptedException {
+        try {
+            return matchService.fetchMatchFromRiotApi(gameId);
+        } catch (HttpClientErrorException.TooManyRequests e){
+            Thread.sleep(120000 + 20000);
+            return matchService.fetchMatchFromRiotApi(gameId);
+        }
+
+    }
+
+    @BeforeStep
+    public void setUp(StepExecution stepExecution) {
+        this.stepExecution = stepExecution;
     }
 }
