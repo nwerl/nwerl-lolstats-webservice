46cdfe1c27b70283185613d48e817397167a5b34
nwerl <msh6242006@gmail.com>
2021-03-14 23:52:37 +0900

    Refactor QueueType
    QueueType was changed Class to Enum.

--- src/main/java/com/nwerl/lolstats/web/dto/riotapi/matchreference/QueueType.java
+++ src/main/java/com/nwerl/lolstats/web/dto/riotapi/matchreference/QueueType.java
@@ -1,11 +1,17 @@
 package com.nwerl.lolstats.web.dto.riotapi.matchreference;
 
-public class QueueType {
-    public static final Integer SOLO_RANK = 420;
-    public static final Integer NORMAL = 430;
-    public static final Integer TEAM_RANK = 440;
-    public static final Integer ARAM = 450;
+import lombok.Getter;
 
+@Getter
+public enum QueueType {
+    SOLO_RANK(420),
+    NORMAL(430),
+    TEAM_RANK(440),
+    ARAM(450);
 
-    private QueueType() {}
+    private int queueCode;
+
+    private QueueType(int queueCode) {
+        this.queueCode = queueCode;
+    }
 }
