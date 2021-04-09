68cb187b981913807699d8f8fbb238e719fcd6fb
nwerl <msh6242006@gmail.com>
2021-04-09 16:03:34 +0900

    Rename ChampionDto to ChampionThumbnailDto

--- src/main/java/com/nwerl/lolstats/web/dto/view/ChampionThumbnailDto.java
+++ src/main/java/com/nwerl/lolstats/web/dto/view/ChampionThumbnailDto.java
@@ -2,12 +2,11 @@ package com.nwerl.lolstats.web.dto.view;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 @Builder
 @Data
-public class ChampionDto {
+public class ChampionThumbnailDto {
     @Id
     private Long id;
     private String name;
