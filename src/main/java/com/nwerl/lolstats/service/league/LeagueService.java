e97d6a801950e616e3e10a18225b4955d448409d
nwerl <msh6242006@gmail.com>
2021-04-09 15:51:43 +0900

    Add findAll with Pageable

--- src/main/java/com/nwerl/lolstats/service/league/LeagueService.java
+++ src/main/java/com/nwerl/lolstats/service/league/LeagueService.java
@@ -7,7 +7,12 @@ import com.nwerl.lolstats.web.dto.riotapi.league.RiotLeagueListDto;
 import com.nwerl.lolstats.web.dto.view.LeagueRankingDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -22,39 +27,51 @@ public class LeagueService {
     private final LeagueItemRepository leagueItemRepository;
     private final LeagueApiCaller leagueApiCaller;
 
+    public final static int RANKING_LIMIT = 300;
+
+    public Page<RiotLeagueItemDto> findAll(Pageable pageable) {
+        Page<LeagueItem> leagueItemPage = leagueItemRepository.findAll(pageable);
+        int totalElements = (int)leagueItemPage.getTotalElements();
+
+        return new PageImpl<>(leagueItemPage.stream().map(LeagueItem::of).collect(Collectors.toList()), pageable, totalElements);
+    }
+
     public RiotLeagueListDto findAll() {
         return new RiotLeagueListDto(leagueItemRepository.findAll().stream().map(LeagueItem::of).collect(Collectors.toList()));
     }
 
-    public void deleteAll() {
-        leagueItemRepository.deleteAll();
+    public void deleteInBatchBySummonerId(List<RiotLeagueItemDto> entities) {
+        leagueItemRepository.deleteInBatchBySummonerId(entities.stream().map(RiotLeagueItemDto::toEntity).collect(Collectors.toList()));
     }
 
     public void deleteBySummonerId(String summonerId) {
         leagueItemRepository.deleteBySummonerId(summonerId);
     }
 
-    //ChallengerLeagueList를 갱신함.
-    public RiotLeagueListDto updateChallengerLeagueList() {
-        List<RiotLeagueItemDto> list = leagueApiCaller.fetchChallengerLeagueListFromRiotApi().getEntries();
-        leagueItemRepository.deleteAll(); //deleteAll 안하려면 N^2로 대조하여 받아온 list에서 없는 DB 데이터 삭제해야 함.
-        leagueItemRepository.saveAll(list.stream().map(RiotLeagueItemDto::toEntity).collect(Collectors.toList()));
-
-        return new RiotLeagueListDto(list);
-    }
 
     public RiotLeagueListDto fetchChallengerLeagueListFromRiotApi() {
         return leagueApiCaller.fetchChallengerLeagueListFromRiotApi();
     }
 
+    @CacheEvict(value = "ranking")
+    public void updateLeagueRanking() {
+        AtomicInteger pageNum = new AtomicInteger(0);
+        Page<RiotLeagueItemDto> leagueItemDto = findAll(PageRequest.of(pageNum.get(), RANKING_LIMIT));
+
+        while(!leagueItemDto.isLast()) {
+            deleteInBatchBySummonerId(leagueItemDto.getContent());
+            leagueItemDto = findAll(PageRequest.of(pageNum.getAndIncrement(), RANKING_LIMIT));
+        }
+    }
 
     @Cacheable(value = "ranking")
     public List<LeagueRankingDto> getLeagueRanking() {
-        AtomicInteger ranking = new AtomicInteger(1);
-
+        AtomicInteger rank = new AtomicInteger(1);
+        Page<LeagueItem> ranking = leagueItemRepository.findAllByOrderByLeaguePointsDesc(PageRequest.of(0, RANKING_LIMIT));
         List<LeagueRankingDto> leagueRankingDtos = new ArrayList<>();
-        for(LeagueItem item : leagueItemRepository.findAllByOrderByLeaguePointsDesc()) {
-            leagueRankingDtos.add(new LeagueRankingDto(ranking.getAndIncrement(), item.getSummonerName(), item.getLeaguePoints()));
+
+        for(LeagueItem item : ranking) {
+            leagueRankingDtos.add(new LeagueRankingDto(rank.getAndIncrement(), item.getSummonerName(), item.getLeaguePoints()));
         }
 
         return leagueRankingDtos;
--- src/main/java/com/nwerl/lolstats/web/domain/league/LeagueItemRepository.java
+++ src/main/java/com/nwerl/lolstats/web/domain/league/LeagueItemRepository.java
@@ -1,10 +1,12 @@
 package com.nwerl.lolstats.web.domain.league;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface LeagueItemRepository extends MongoRepository<LeagueItem, String> {
-    public List<LeagueItem> findAllByOrderByLeaguePointsDesc();
+    public Page<LeagueItem> findAll(Pageable pageable);
+    public Page<LeagueItem> findAllByOrderByLeaguePointsDesc(Pageable pageable);
     public void deleteBySummonerId(String summonerId);
+    public void deleteInBatchBySummonerId(Iterable<LeagueItem> entities);
 }
