3ae759f8a62c308d7d5e5e5fa4f9560d97c356e5
nwerl <msh6242006@gmail.com>
2021-04-09 16:07:53 +0900

    Rename LeagueItemMap member

--- src/main/java/com/nwerl/lolstats/batch/LeagueItemMap.java
+++ src/main/java/com/nwerl/lolstats/batch/LeagueItemMap.java
@@ -15,34 +15,36 @@ public class LeagueItemMap {
     private final LeagueService leagueService;
     private final SummonerService summonerService;
 
-    private Map<String, String> summonerNameList;
+    private Map<String, String> leagueItemMap;
     private Iterator<String> it;
 
     public LeagueItemMap(LeagueService leagueService,
                          SummonerService summonerService) {
         this.leagueService = leagueService;
         this.summonerService = summonerService;
-        this.summonerNameList = new HashMap<>();
+        this.leagueItemMap = new HashMap<>();
+
+        update();
     }
 
 
     public String getNextSummonerName() {
         if(!it.hasNext())
-            it = summonerNameList.keySet().iterator();
+            it = leagueItemMap.keySet().iterator();
 
         return it.next();
     }
 
     public String getAccountId(String summonerName) {
-        return summonerNameList.get(summonerName);
+        return leagueItemMap.get(summonerName);
     }
 
     public void update() {
-        summonerNameList = leagueService.findAll().getEntries().stream()
+        leagueItemMap = leagueService.findAll().getEntries().stream()
                 .collect(Collectors.toMap(RiotLeagueItemDto::getSummonerName,
                         item -> summonerService.findAccountIdById(item.getSummonerId())));
         //todo : 이부분 summonerName으로 찾으면 에러나는거 봐서는 닉변수정이 바로 안되는듯함
 
-        it = summonerNameList.keySet().iterator();
+        it = leagueItemMap.keySet().iterator();
     }
 }
