282d84d7dc8774ab9a228d702bf8d637e972afd1
nwerl <msh6242006@gmail.com>
2021-04-13 18:07:24 +0900

    Refactor MatchService
    Use redis sorted set as Match Cache.
    Fix bugs.

--- src/main/java/com/nwerl/lolstats/service/match/MatchService.java
+++ src/main/java/com/nwerl/lolstats/service/match/MatchService.java
@@ -1,43 +1,46 @@
 package com.nwerl.lolstats.service.match;
 
 import com.nwerl.lolstats.service.summoner.SummonerService;
-import com.nwerl.lolstats.web.domain.match.MatchListRepository;
+import com.nwerl.lolstats.web.domain.match.MatchReferenceRepository;
 import com.nwerl.lolstats.web.domain.match.MatchRepository;
 import com.nwerl.lolstats.web.dto.riotapi.match.RiotMatchDto;
 import com.nwerl.lolstats.web.dto.riotapi.matchreference.QueueType;
 import com.nwerl.lolstats.web.dto.riotapi.matchreference.RiotMatchReferenceDto;
 import com.nwerl.lolstats.web.dto.view.MatchDto;
-import com.nwerl.lolstats.web.dto.view.MatchListDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class MatchService {
     private final MatchRepository matchRepository;
-    private final MatchListRepository matchListRepository;
+    private final MatchReferenceRepository matchReferenceRepository;
     private final MatchApiCaller matchApiCaller;
     private final SummonerService summonerService;
     private final RedisTemplate<String, MatchDto> redisTemplate;
 
-    private ListOperations<String, MatchDto> listOps;
+    private ZSetOperations<String, MatchDto> zSetOps;
+
+    public static final int CACHE_LIMIT = 1000;
 
     @PostConstruct
     public void init() {
-        listOps = redisTemplate.opsForList();
+        zSetOps = redisTemplate.opsForZSet();
     }
 
     public Boolean existsByGameId(Long gameId) {
-        return matchRepository.existsByGameId(gameId);
+        return matchRepository.existsById(gameId);
     }
 
     public RiotMatchReferenceDto fetchLastRankMatchReferenceFromRiotApi(String accountId) {
@@ -54,56 +57,31 @@ public class MatchService {
         return matchApiCaller.fetchMatchFromRiotApi(gameId);
     }
 
-    public MatchListDto getMatchListByName(String name) {
-        String id = summonerService.findAccountIdByName(name);
-
-        return matchListRepository.findByAccountId(id).of();
-    }
-
-    public MatchDto getMatchBySummonerName(String summonerName, Long id) {
-        String accountId = summonerService.findAccountIdByName(summonerName);
-        return matchRepository.findById(id).map(m -> m.of(accountId)).orElse(MatchDto.builder().build());
-    }
-
-    public MatchDto getMatchByAccountId(String accountId, Long id) {
-        return matchRepository.findById(id).map(m -> m.of(accountId)).orElse(MatchDto.builder().build());
-    }
-
-    public Boolean existsBySummonerName(String name) {
-        String accountId = summonerService.findAccountIdByName(name);
-        return matchListRepository.existsByAccountId(name);
-    }
-
     public List<MatchDto> getMatchesByName(String name) {
         String accountId = summonerService.findAccountIdByName(name);
-        Long size = listOps.size(accountId);
+        Long size = zSetOps.size(accountId);
 
         if(size == null || size == 0) {
-            return setMatchCache(accountId);
+            setMatchCache(accountId);
         }
 
         return getMatchCache(accountId);
     }
 
-    private List<MatchDto> setMatchCache(String accountId) {
-        List<MatchListDto.MatchReferenceDto> matchReferences = matchListRepository.findByAccountId(accountId).of().getMatchReferences();
+    public void setMatchCache(String accountId) {
+        List<MatchDto> list = matchReferenceRepository.findAllBySummoner_AccountId(accountId, PageRequest.of(0, CACHE_LIMIT))
+                .stream().map(m -> m.getMatch().of(accountId)).collect(Collectors.toList());
 
-        List<MatchDto> list = new ArrayList<>();
-        for(MatchListDto.MatchReferenceDto item : matchReferences) {
-            list.add(getMatchByAccountId(accountId, item.getGameId()));
-        }
-
-        listOps.rightPushAll(accountId, list);
-
-        return list;
+        for(MatchDto dto : list)
+            zSetOps.add(accountId, dto, dto.getGameId());
     }
 
-    private List<MatchDto> getMatchCache(String accountId) {
-        Long size = listOps.size(accountId);
+    public List<MatchDto> getMatchCache(String accountId) {
+        Long size = zSetOps.size(accountId);
 
         if(size == null || size == 0)
             return Collections.emptyList();
 
-        return listOps.range(accountId, 0, size);
+        return new ArrayList<>(zSetOps.reverseRange(accountId, 0, size));
     }
 }
