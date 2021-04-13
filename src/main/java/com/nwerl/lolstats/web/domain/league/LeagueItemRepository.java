232d1357dc85c404a3bc010a2cf5b4b1d2598d2b
nwerl <msh6242006@gmail.com>
2021-04-13 17:55:43 +0900

    Modify LeagueItem Entity
    Use JPA, not mongoDB

--- src/main/java/com/nwerl/lolstats/web/domain/league/LeagueItemRepository.java
+++ src/main/java/com/nwerl/lolstats/web/domain/league/LeagueItemRepository.java
@@ -2,11 +2,11 @@ package com.nwerl.lolstats.web.domain.league;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LeagueItemRepository extends MongoRepository<LeagueItem, String> {
+public interface LeagueItemRepository extends JpaRepository<LeagueItem, Long> {
     public Page<LeagueItem> findAll(Pageable pageable);
     public Page<LeagueItem> findAllByOrderByLeaguePointsDesc(Pageable pageable);
     public void deleteBySummonerId(String summonerId);
-    public void deleteInBatchBySummonerId(Iterable<LeagueItem> entities);
+    public void deleteInBatch(Iterable<LeagueItem> entities);
 }
