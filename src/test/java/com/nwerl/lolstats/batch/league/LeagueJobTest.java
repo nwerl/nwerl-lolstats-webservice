40add259baed3ab9826392ac82bf2bb72d793c3c
nwerl <msh6242006@gmail.com>
2021-05-06 23:43:59 +0900

    Refactor Batch integration tests

--- src/test/java/com/nwerl/lolstats/batch/league/LeagueJobTest.java
+++ src/test/java/com/nwerl/lolstats/batch/league/LeagueJobTest.java
@@ -2,22 +2,28 @@ package com.nwerl.lolstats.batch.league;
 
 import com.nwerl.lolstats.batch.BatchApplication;
 import com.nwerl.lolstats.service.league.LeagueApiCaller;
+import com.nwerl.lolstats.service.summoner.SummonerApiCaller;
+import com.nwerl.lolstats.web.domain.league.LeagueItemRepository;
+import com.nwerl.lolstats.web.domain.summoner.SummonerRepository;
+import com.nwerl.lolstats.web.dto.riotapi.league.RiotLeagueItemDto;
+import com.nwerl.lolstats.web.dto.riotapi.league.RiotLeagueListDto;
+import com.nwerl.lolstats.web.dto.riotapi.summoner.RiotSummonerDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.ExitStatus;
-import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.HttpServerErrorException;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -26,27 +32,70 @@ import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @MockBean(BatchApplication.class)
-@EnableAutoConfiguration
+@TestPropertySource(locations="classpath:application-test.properties")
 @SpringBootTest(classes = {LeagueJobTestConfiguration.class})
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class LeagueJobTest {
-    @Autowired
-    @Qualifier("leagueJob")
-    private Job job;
-
     @Autowired
     @Qualifier("leagueJobUtils")
     private JobLauncherTestUtils jobLauncherTestUtils;
 
+    @Autowired
+    private LeagueItemRepository leagueItemRepository;
+    @Autowired
+    private SummonerRepository summonerRepository;
     @MockBean
     private LeagueApiCaller leagueApiCaller;
+    @MockBean
+    private SummonerApiCaller summonerApiCaller;
+
+    public final static int TEST_DATA_LIMIT = 10;
 
     @Test
-    public void leagueJob_Retry_Test() throws Exception {
-        JobLauncher jobLauncher = jobLauncherTestUtils.getJobLauncher();
-        given(leagueApiCaller.fetchChallengerLeagueListFromRiotApi()).willThrow(HttpServerErrorException.class);
-        JobExecution jobExecution = jobLauncher.run(job, new JobParametersBuilder().addLong("time", System.currentTimeMillis()).toJobParameters());
-        ExitStatus exitStatus = jobExecution.getExitStatus();
-        assertThat(exitStatus.getExitCode(), is("COMPLETED"));
+    public void leagueJob_Integeration_Test() throws Exception {
+        //given
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("time", String.valueOf(System.currentTimeMillis()))
+                .toJobParameters();
+
+        RiotLeagueListDto riotLeagueListDto = RiotLeagueListDto.builder().entries(getRiotLeagueItems()).build();
+        given(leagueApiCaller.fetchChallengerLeagueListFromRiotApi()).willReturn(riotLeagueListDto);
+        for(int i=1;i<=TEST_DATA_LIMIT;i++) {
+            RiotSummonerDto riotSummonerDto = getRiotSummonerDtosById(i);
+            given(summonerApiCaller.fetchSummonerFromRiotApiById(Integer.toString(i))).willReturn(riotSummonerDto);
+        }
+
+        //when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        //then
+        assertThat(jobExecution.getExitStatus(), is(ExitStatus.COMPLETED));
+        for(int i=1;i<=TEST_DATA_LIMIT;i++) {
+            assertThat(leagueItemRepository.findById((long) i).get().getSummonerId(), is(Integer.toString(i)));
+            assertThat(summonerRepository.findById((long) i).get().getSummonerId(), is(Integer.toString(i)));
+        }
+    }
+
+    private RiotSummonerDto getRiotSummonerDtosById(int id) {
+        return RiotSummonerDto.builder()
+                .id(Integer.toString(id))
+                .accountId(Integer.toString(id))
+                .name(Character.toChars(id).toString())
+                .build();
+    }
+
+
+    private List<RiotLeagueItemDto> getRiotLeagueItems() {
+        List<RiotLeagueItemDto> riotLeagueItems = new LinkedList<>();
+
+        for(int i=1;i<=TEST_DATA_LIMIT;i++) {
+            riotLeagueItems.add(RiotLeagueItemDto.builder()
+                    .summonerId(Integer.toString(i))
+                    .summonerName(Character.toChars(i).toString())
+                    .leaguePoints(i)
+                    .build()
+            );
+        }
+
+        return riotLeagueItems;
     }
 }
--- src/test/java/com/nwerl/lolstats/batch/match/MatchJobTestConfiguration.java
+++ src/test/java/com/nwerl/lolstats/batch/match/MatchJobTestConfiguration.java
@@ -5,14 +5,15 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @TestConfiguration
+@EnableAutoConfiguration
 @EnableBatchProcessing
 public class MatchJobTestConfiguration {
     @Bean
-    @Qualifier("matchJob")
     JobLauncherTestUtils leagueJobUtils() {
         return new JobLauncherTestUtils(){
             @Override
