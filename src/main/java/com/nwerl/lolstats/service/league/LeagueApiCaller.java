7992061d48ab2a429ba502a4d9372bcba8c2b363
nwerl <msh6242006@gmail.com>
2021-05-06 23:38:27 +0900

    Modify LeagueApiCaller to remove @Retryable annotation

--- src/main/java/com/nwerl/lolstats/service/league/LeagueApiCaller.java
+++ src/main/java/com/nwerl/lolstats/service/league/LeagueApiCaller.java
@@ -1,10 +1,8 @@
 package com.nwerl.lolstats.service.league;
 
 import com.nwerl.lolstats.web.dto.riotapi.league.RiotLeagueListDto;
-import org.springframework.retry.annotation.Retryable;
-import org.springframework.web.client.HttpServerErrorException;
 
-@Retryable(value = HttpServerErrorException.class, maxAttempts = 3)
+
 public interface LeagueApiCaller {
     public static final String CHALLENGER_LEAGUE_URI = "/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5";
 
