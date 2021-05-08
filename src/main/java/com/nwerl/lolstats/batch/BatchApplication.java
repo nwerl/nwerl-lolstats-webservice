ece57e65de72b8d847c2cb3e5e8923c50333142b
nwerl <msh6242006@gmail.com>
2021-05-09 01:02:05 +0900

    Remove initLeagueList

--- src/main/java/com/nwerl/lolstats/batch/BatchApplication.java
+++ src/main/java/com/nwerl/lolstats/batch/BatchApplication.java
@@ -13,11 +13,11 @@ public class BatchApplication {
     private final BatchJobLauncher batchJobLauncher;
     private final LeagueItemMap leagueItemMap;
 
-    @Scheduled(fixedDelay = Long.MAX_VALUE)
-    public void initLeagueList() throws Exception {
-        batchJobLauncher.leagueLaunch();
-        leagueItemMap.update();
-    }
+//    @Scheduled(fixedDelay = Long.MAX_VALUE)
+//    public void initLeagueList() throws Exception {
+//        batchJobLauncher.leagueLaunch();
+//        leagueItemMap.update();
+//    }
 
     @Scheduled(cron = "0 0 * * * *")
     public void leagueJobExecute() throws Exception {
