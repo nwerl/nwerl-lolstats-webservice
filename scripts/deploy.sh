24fe35f215c14d8e68c049b631b32f062bf58b5b
nwerl <msh6242006@gmail.com>
2021-05-10 22:54:01 +0900

    Modify deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-APP=/home/ec2-user/app
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=nwerl-lolstats-webservice
-CLASSPATH=$REPOSITORY/$PROJECT_NAME/src/main/resources
 
 echo "> Build 파일 복사"
 
@@ -13,17 +11,17 @@ echo "> 현재 구동중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl nwerl-lolstats-webservice | grep jar | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-   echo "> kill -15 $CURRENT_PID"
-   kill -15 $CURRENT_PID
-   sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
