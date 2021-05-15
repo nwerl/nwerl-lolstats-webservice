fb57c97bd1ad87ab4da102875401583e5c3e16b3
nwerl <msh6242006@gmail.com>
2021-05-15 15:39:14 +0900

    Fix typo

--- scripts/start.sh
+++ scripts/start.sh
@@ -28,6 +28,8 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE로 실행합니다."
 
+adasdad
+
 nohup java -jar \
    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-redis.properties,/home/ec2-user/app/application-riotapi.properties,/home/ec2-user/app/application-filepath.properties,/home/ec2-user/app/application-real-mysql.properties,/home/ec2-user/app/application-ssl.properties \
    -Dspring.profiles.active=$IDLE_PROFILE \
