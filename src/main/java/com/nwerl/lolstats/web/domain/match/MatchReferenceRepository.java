496ee4dcdd730b80ab1e834b8d3a143a09f26dde
nwerl <msh6242006@gmail.com>
2021-06-16 23:53:48 +0900

    Modify MatchReferenceRepository to add methods

--- src/main/java/com/nwerl/lolstats/web/domain/match/MatchReferenceRepository.java
+++ src/main/java/com/nwerl/lolstats/web/domain/match/MatchReferenceRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MatchReferenceRepository extends JpaRepository<MatchReference, Long> {
     public Page<MatchReference> findAllBySummoner_AccountId(String accountId, Pageable pageable);
+    public List<MatchReference> findFirst20BySummoner_AccountIdAndGameCreationLessThanOrderByGameCreationDesc(String accountId, Long gameCreation);
+    public MatchReference findFirstBySummoner_AccountIdOrderByGameCreationDesc(String accountId);
 }
