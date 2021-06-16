ec2422155c448baad85e657e8f58135db055b7e0
nwerl <msh6242006@gmail.com>
2021-06-16 23:51:57 +0900

    Modify WebConfig to use WebAutoConfiguration

--- src/main/java/com/nwerl/lolstats/config/WebConfig.java
+++ src/main/java/com/nwerl/lolstats/config/WebConfig.java
@@ -2,10 +2,10 @@ package com.nwerl.lolstats.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig extends WebMvcConfigurationSupport {
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        registry.addResourceHandler("/**")
