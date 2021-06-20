64446fdba841e6e4e582e1d0f2653e925cb4be7c
nwerl <msh6242006@gmail.com>
2021-06-20 22:42:09 +0900

    Modify MatchReferenceRepository

--- src/main/java/com/nwerl/lolstats/web/domain/match/MatchReferenceRepository.java
+++ src/main/java/com/nwerl/lolstats/web/domain/match/MatchReferenceRepository.java
@@ -9,5 +9,6 @@ import java.util.List;
 public interface MatchReferenceRepository extends JpaRepository<MatchReference, Long> {
     public Page<MatchReference> findAllBySummoner_AccountId(String accountId, Pageable pageable);
     public List<MatchReference> findFirst20BySummoner_AccountIdAndGameCreationLessThanOrderByGameCreationDesc(String accountId, Long gameCreation);
+    public List<MatchReference> findFirst20BySummoner_AccountIdOrderByGameCreationDesc(String accountId);
     public MatchReference findFirstBySummoner_AccountIdOrderByGameCreationDesc(String accountId);
 }
