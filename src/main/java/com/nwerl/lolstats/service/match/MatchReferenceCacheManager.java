c62bbb90258f87f9d8bd6fe9eeee29da9093c7cb
nwerl <msh6242006@gmail.com>
2021-06-20 22:41:18 +0900

    Modify MatchReferenceCacheManager
    Add new Cache 'firstMatchPage' that set in 1h TTL.

--- src/main/java/com/nwerl/lolstats/service/match/MatchReferenceCacheManager.java
+++ src/main/java/com/nwerl/lolstats/service/match/MatchReferenceCacheManager.java
@@ -1,5 +1,6 @@
 package com.nwerl.lolstats.service.match;
 
+import com.nwerl.lolstats.web.domain.match.MatchReference;
 import com.nwerl.lolstats.web.domain.match.MatchReferenceRepository;
 import com.nwerl.lolstats.web.dto.view.MatchDto;
 import lombok.RequiredArgsConstructor;
@@ -17,15 +18,28 @@ import java.util.stream.Collectors;
 public class MatchReferenceCacheManager {
     private final MatchReferenceRepository matchReferenceRepository;
 
-    @Cacheable(value="match", key="#accountId.concat('-').concat(#gameCreation)")
+    @Cacheable(value="matchPage", key="#accountId.concat('-').concat(#gameCreation)")
     public List<MatchDto> getMatchPageWithNoOffset(String accountId, Long gameCreation) {
         return matchReferenceRepository.findFirst20BySummoner_AccountIdAndGameCreationLessThanOrderByGameCreationDesc(accountId, gameCreation)
                 .stream().map(m -> m.getMatch().of(accountId)).collect(Collectors.toList());
     }
 
-    @CachePut(value="match", key="#accountId.concat('-').concat(#gameCreation)")
+    @CachePut(value="matchPage", key="#accountId.concat('-').concat(#gameCreation)")
     public List<MatchDto> setMatchPageWithNoOffset(String accountId, Long gameCreation) {
         return matchReferenceRepository.findFirst20BySummoner_AccountIdAndGameCreationLessThanOrderByGameCreationDesc(accountId, gameCreation)
+                .stream().map(m -> m.getMatch().of(accountId)).collect(Collectors.toList());
+    }
+
+    @Cacheable(value="firstMatchPage", key="#accountId")
+    public List<MatchDto> getFirstMatchPage(String accountId) {
+        return matchReferenceRepository.findFirst20BySummoner_AccountIdOrderByGameCreationDesc(accountId)
+                .stream().map(m -> m.getMatch().of(accountId)).collect(Collectors.toList());
+    }
+
+
+    @CachePut(value="firstMatchPage", key="#accountId")
+    public List<MatchDto> setFirstMatchPage(String accountId) {
+        return matchReferenceRepository.findFirst20BySummoner_AccountIdOrderByGameCreationDesc(accountId)
                 .stream().map(m -> m.getMatch().of(accountId)).collect(Collectors.toList());
     }
 }
